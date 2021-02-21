2e81d7b66926ea37917d349ba461e6765472b014
enstrom480b <64636784+enstrom480b@users.noreply.github.com>
2021-02-21 12:19:08 -0600

    Add files via upload
    table-cell-display property

---
+++ tablecell.php
@@ -0,0 +1,136 @@
+<div class="navbar">
+<p>
+Measures to control the illicit movement of narcotics and psychotropic substances by air.
+Non-restricted area Areas of an airport to which the public has access or to which access is otherwise unrestricted.
+Off airport processing facilities
+A passenger or cargo linked terminal at an urban population centre at which processing facilities are provided.
+Operator
+A person, organization or enterprise engaged in or offering to engage in an aircraft operation. 
+Passenger area
+All the ground space and facilities provided for passenger processing. It includes aprons, passenger buildings, vehicle parks and roads.
+Permits
+Cards or other documentation issued to individual persons employed on airports or who otherwise have need for authorized access to airports or to any restricted part(s) thereof, for the purpose of facilitating access and identifying the individual and includes vehicle documentation issued for similar purposes.
+Permits are sometimes referred to as Airport Identity/Movement Cards or Passes
+Pier
+A corridor at above or below ground level to connect aircraft stands to a passenger building.
+Prohibited Articles
+These are articles which passengers should not be allowed (without valid, lawful reason) to take with them on board an aircraft because they would constitute a threat to the security of the aircraft
+Regulated agent
+An agent, freight forwarder or any other entity that conducts business with an operator and provides security controls that are 
+</p>
+</div>
+<div class="outer">
+<div class="leftmargin">
+<p>
+Measures to control the illicit movement of narcotics and psychotropic substances by air.
+Non-restricted area Areas of an airport to which the public has access or to which access is otherwise unrestricted.
+Off airport processing facilities
+A passenger or cargo linked terminal at an urban population centre at which processing facilities are provided.
+Operator
+A person, organization or enterprise engaged in or offering to engage in an aircraft operation. 
+Passenger area
+All the ground space and facilities provided for passenger processing. It includes aprons, passenger buildings, vehicle parks and roads.
+Permits
+Cards or other documentation issued to individual persons employed on airports or who otherwise have need for authorized access to airports or to any restricted part(s) thereof, for the purpose of facilitating access and identifying the individual and includes vehicle documentation issued for similar purposes.
+Permits are sometimes referred to as Airport Identity/Movement Cards or Passes
+Pier
+A corridor at above or below ground level to connect aircraft stands to a passenger building.
+Prohibited Articles
+These are articles which passengers should not be allowed (without valid, lawful reason) to take with them on board an aircraft because they would constitute a threat to the security of the aircraft
+Regulated agent
+An agent, freight forwarder or any other entity that conducts business with an operator and provides security controls that are 
+</p>
+</div>
+<div class="centermargin">
+<p>
+Measures to control the illicit movement of narcotics and psychotropic substances by air.
+Non-restricted area Areas of an airport to which the public has access or to which access is otherwise unrestricted.
+Off airport processing facilities
+A passenger or cargo linked terminal at an urban population centre at which processing facilities are provided.
+Operator
+A person, organization or enterprise engaged in or offering to engage in an aircraft operation. 
+Passenger area
+All the ground space and facilities provided for passenger processing. It includes aprons, passenger buildings, vehicle parks and roads.
+Permits
+Cards or other documentation issued to individual persons employed on airports or who otherwise have need for authorized access to airports or to any restricted part(s) thereof, for the purpose of facilitating access and identifying the individual and includes vehicle documentation issued for similar purposes.
+Permits are sometimes referred to as Airport Identity/Movement Cards or Passes
+Pier
+A corridor at above or below ground level to connect aircraft stands to a passenger building.
+Prohibited Articles
+These are articles which passengers should not be allowed (without valid, lawful reason) to take with them on board an aircraft because they would constitute a threat to the security of the aircraft
+Regulated agent
+An agent, freight forwarder or any other entity that conducts business with an operator and provides security controls that are 
+</p>
+</div>
+
+<div class="rightmargin">
+<p>
+Measures to control the illicit movement of narcotics and psychotropic substances by air.
+Non-restricted area Areas of an airport to which the public has access or to which access is otherwise unrestricted.
+Off airport processing facilities
+A passenger or cargo linked terminal at an urban population centre at which processing facilities are provided.
+Operator
+A person, organization or enterprise engaged in or offering to engage in an aircraft operation. 
+Passenger area
+All the ground space and facilities provided for passenger processing. It includes aprons, passenger buildings, vehicle parks and roads.
+Permits
+Cards or other documentation issued to individual persons employed on airports or who otherwise have need for authorized access to airports or to any restricted part(s) thereof, for the purpose of facilitating access and identifying the individual and includes vehicle documentation issued for similar purposes.
+Permits are sometimes referred to as Airport Identity/Movement Cards or Passes
+Pier
+A corridor at above or below ground level to connect aircraft stands to a passenger building.
+Prohibited Articles
+These are articles which passengers should not be allowed (without valid, lawful reason) to take with them on board an aircraft because they would constitute a threat to the security of the aircraft
+Regulated agent
+An agent, freight forwarder or any other entity that conducts business with an operator and provides security controls that are 
+</p>
+</div>
+
+
+</div>
+<div class="footer">
+
+</div>
+
+
+
+<style>
+*{
+margin:0;
+padding:0;
+border-sizing:border-box;
+}
+.outer{
+width:100vw;
+height:80vh;
+background-color:coral;
+display:table;
+}
+.navbar{
+display:table-cell;
+height:10vh;
+width:100vw;
+background-color:pink;	
+}
+.centermargin{
+width:40vw;
+background-color:coral;
+display:table-cell;
+}
+.leftmargin{
+display:table-cell;
+width:30vw;
+background-color:red;
+}
+.footer{
+display:table-cell;
+height:10vh;
+width:100vw;
+background-color:pink;	
+}
+.rightmargin{
+display:table-cell;
+width:30vw;
+background-color:red;
+}
+
+</style>
